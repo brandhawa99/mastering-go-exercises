9f4c9249909a3c4129186649070d0217d50cc60f
brandhawa99 <baltej.randhawa0@gmail.com>
2024-03-27 17:09:01 -0700

    ex1

---
+++ 2-chapter/exercises/q1/callCLib/ex1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void coolName(char *fName)
+{
+  printf("Wow you got a cool name %s", fName);
+}
---
+++ 2-chapter/exercises/q1/callCLib/ex1.h
@@ -0,0 +1,5 @@
+#ifndef EX1_H
+#define EX1_H
+
+void coolName(char *fName)
+#endif
