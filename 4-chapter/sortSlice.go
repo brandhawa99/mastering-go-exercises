24e6886b331b582f0bc6e3712b0f914775af2768
brandhawa99 <baltej.randhawa0@gmail.com>
2024-04-06 22:17:57 -0700

    structs

---
+++ 4-chapter/sortSlice.go
@@ -0,0 +1,14 @@
+package main
+
+type aStructure struct {
+	person string
+	height int
+	weight int
+}
+
+func main() {
+	var s1 aStructure
+	p1 := aStructure("fmt", 12, -2)
+	p2 := aStructure(weight: 12, height: -2)
+
+}
