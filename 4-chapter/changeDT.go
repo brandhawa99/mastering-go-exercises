5be36f4f173c7e64690707c53f9968a6a9c4491e
brandhawa99 <baltej.randhawa0@gmail.com>
2024-04-09 23:04:02 -0700

    date time regex

--- 4-chapter/changeDT.go
+++ 4-chapter/changeDT.go
@@ -34,19 +34,30 @@ func main() {
 		} else if err != nil {
 			fmt.Printf("error reading file %s", err)
 		}
-		r1 := regexp.MustCompile('.*\[(\d\d\/\w+/\d\d\d\d:\d\d:\d\d:\d\d.*)\] .*')
-	
-		if r1.MatchString(line){
+		r1 := regexp.MustCompile(`.*\[(\d\d\/\w+/\d\d\d\d:\d\d:\d\d:\d\d.*)\] .*`)
+		if r1.MatchString(line) {
 			match := r1.FindStringSubmatch(line)
 			d1, err := time.Parse("02/Jan/2006:15:04:05 -0700", match[1])
-			if err == nil{
+			if err == nil {
 				newFormat := d1.Format(time.Stamp)
-				fmt.Print(strings.Replace(line, match[1], newFormat,1))
-			}else{
-				notAMatch ++
+				fmt.Print(strings.Replace(line, match[1], newFormat, 1))
+			} else {
+				notAMatch++
+			}
+			continue
+		}
+		r2 := regexp.MustCompile(`.*\[(\w+\-\d\d-\d\d:\d\d:\d\d:\d\d.*)\] .*`)
+		if r2.MatchString(line) {
+			match := r2.FindStringSubmatch(line)
+			d1, err := time.Parse("Jan-02-06:15:04:05 -0700", match[1])
+			if err == nil {
+				newFormat := d1.Format(time.Stamp)
+				fmt.Print(strings.Replace(line, match[1], newFormat, 1))
+			} else {
+				notAMatch++
 			}
 			continue
 		}
 	}
-
+	fmt.Println(notAMatch, "Lines did not match!")
 }
