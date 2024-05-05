b8120f96f85306d8610648b064283950d95151d9
brandhawa99 <baltej.randhawa0@gmail.com>
2024-05-05 02:06:53 -0700

    container list

--- 5-chapter/container/list/conList.go
+++ 5-chapter/container/list/conList.go
@@ -1,7 +1,19 @@
 package main
 
-import "container/list"
+import (
+	"container/list"
+	"fmt"
+)
 
 func printList(l *list.List) {
+	for t := l.Back(); t != nil; t = t.Prev() {
+		fmt.Print(t.Value, " ")
+	}
+	fmt.Println()
+
+	for t := l.Front(); t != nil; t = t.Next() {
+		fmt.Print(t.Value, " ")
+	}
+	fmt.Println()
 
 }
